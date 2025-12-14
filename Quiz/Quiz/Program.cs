d3a5db927bc60280c38e119aaec10be9de506f10
Henes0 <heneshenes99@gmail.com>
2025-12-14 09:27:57 +0100

    14-12-2025

--- Quiz/Quiz/Program.cs
+++ Quiz/Quiz/Program.cs
@@ -1,1 +1,43 @@
-﻿Console.WriteLine(" Witaj w quizie przed tobą 7 pytań!");
+﻿using Quiz;
+using System.Runtime.Intrinsics.X86;
+
+// Powołanie do życia obiektu game - który będzie zarządzał przebiegiem gry
+var game = new Game();
+
+// Wyświetlenie komunikatu powitalnego
+game.DisplayWelcomeMessage();
+
+// Zorganizowanie wszystkich pytań w grze
+game.CreateQuestionDatabase();
+
+// Losowanie pytania z najniżej kategorii
+game.DrawQuestionFromCurrentCategory();
+
+// Wyświetlanie pytania
+game.CurrentQuestion.Display();
+
+//pobranie numeru odpowiedzi od gracza
+var userNumber = Console.ReadLine();
+
+//Sprawdzanie odpowiedzi
+var IsCorrect = game.CheckUserAnswer(userNumber);
+
+Console.Clear();
+if (IsCorrect)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Brawo to jest poprawna odpowiedź!");
+    
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("niestety to jest zła odpowiedź");
+    Console.WriteLine("GAME OVER");
+}
+
+
+
+
+
+Console.ReadLine();
--- Quiz/Quiz/question.cs
+++ Quiz/Quiz/question.cs
@@ -1,10 +1,28 @@
 namespace Quiz;
 
-public class question
+public class Question
 {
     public int Id { get; set; }
     public int Category { get; set; }
     public string Content { get; set; }
-    public string Answer { get; set; }
+    public List<Answer> Answers { get; set; } = new List<Answer>();
 
+    public void Display()
+    {
+        Console.WriteLine($"Pytanie za {Category} pkt");
+        Console.WriteLine();
+        Console.WriteLine($"{Content}");
+        Console.WriteLine();
+        int answernumber = 1;
+        foreach (var answer in Answers)
+        {
+            Console.WriteLine($" {answer.Id}. {answer.Content}");
+            answernumber++;
+        }
+        Console.WriteLine();
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("Wybierz prawidłową odpowiedź");
+        Console.WriteLine("Naciśnij 1,2,3 lub 4 żeby wybrać odpowiedź");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
 }
